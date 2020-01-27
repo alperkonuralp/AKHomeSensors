dedeb37b74f93a6da9a944efc380facbfe7038a3
Alper Konuralp <alper@konuralp.gen.tr>
2020-01-27 21:08:14 +0300

    I fixed some bugs

--- src/client/src/db.js
+++ src/client/src/db.js
@@ -3,13 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCwSbtO1A5PkysRaaenyjRFj2yuAtfbUOY",
+    apiKey: "AIzaSyCwSbtO1A5PkysRaaenyjRFj2yuAtfbUOyy",
     authDomain: "akhomesensors.firebaseapp.com",
     databaseURL: "https://akhomesensors.firebaseio.com",
     projectId: "akhomesensors",
     storageBucket: "akhomesensors.appspot.com",
-    messagingSenderId: "1081250432958",
-    appId: "1:1081250432958:web:5cd1e8042c8a489403dbef"
+    messagingSenderId: "10812504329589",
+    appId: "1:1081250432958:web:5cd1e8042c8a489403dbeff"
 };
 
 export const db = firebase
